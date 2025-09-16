315f9469448ce17291651d60da83ed4892b95b3a
satyamrajawat-1 <satyamrajawat283123@gmail.com>
2025-09-17 00:03:42 +0530

    changing callback url

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${BASE_URL}/auth/google/callback`,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
